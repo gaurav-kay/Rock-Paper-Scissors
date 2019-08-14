31ddaf2d4d9b3d3e86037ed49d651af1fa2bb327
Gaurav Kashyap <gauravandkashyap@gmail.com>
2019-08-14 20:53:42 +0530

    Added room joining functionality

--- app/src/main/java/com/example/rockpaperscissors/LoadingScreenActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/LoadingScreenActivity.java
@@ -2,24 +2,52 @@ package com.example.rockpaperscissors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
+
 public class LoadingScreenActivity extends AppCompatActivity {
     private static final String TAG = "TAG";
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
 
-        Bundle bundle = getIntent().getExtras();
+        final Bundle bundle = getIntent().getExtras();
+
+        Log.d(TAG, "onCreate: in loading screen activity");
 
         ProgressBar progressBar = findViewById(R.id.progressBar);
         TextView textView = findViewById(R.id.textView);
         String textViewText = "Waiting for " + bundle.getString("username") + "'s response";
         textView.setText(textViewText);
+
+        db.collection("rooms")
+                .document((String) bundle.get("roomId"))
+
+                .addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                        if ((boolean) documentSnapshot.get("isFull")) {
+                            Intent newActivityIntent = new Intent(LoadingScreenActivity.this, GameActivity.class);
+                            newActivityIntent.putExtra("roomId", (String) bundle.get("roomId"));
+
+                            startActivity(newActivityIntent);
+                            finish();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -22,15 +22,19 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.annotation.Nullable;
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
 
     private ArrayList<String> usernames = new ArrayList<>();
+    private ArrayList<String> listViewDetails = new ArrayList<>();
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -73,41 +78,21 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                joinRoomAsOpponent(usernames.get(i));
 
-                Toast.makeText(MainActivity.this, "clicc", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onItemClick: clicc");
+                Log.d(TAG, "onItemClick: " + usernames.get(i));
             }
         });
-    }
-
-    private void getPendingInvites() {
-        db.collection("users")
-                .document(getCurrentUsername())
-                .collection("invites")
-
-                .addSnapshotListener(this, new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        if (queryDocumentSnapshots != null) {
-                            for (QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots) {
-                                usernames.add((String) queryDocumentSnapshot.get("by"));
-                            }
-
-                            ArrayAdapter arrayAdapter =
-                                    new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, usernames);
-
-                            listView.setAdapter(arrayAdapter);
-                        }
-                    }
-                });
     }
 
     private void sendInvite(final String username) {
         Map<String, Object> map = new HashMap<>();
         map.put("by", getCurrentUsername());
+        map.put("at", new SimpleDateFormat("h:mm a", Locale.ENGLISH).format(new Date()));
 
         // TODO: create room
 
+        final String roomId = createRoomAsHost(username);
 
         // TODO: add firebase admin sdk
 
@@ -123,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                         public void onSuccess(Void aVoid) {
                             Intent newActivityIntent = new Intent(MainActivity.this, LoadingScreenActivity.class);
                             newActivityIntent.putExtra("username", username);
+                            newActivityIntent.putExtra("roomId", roomId);
 
                             startActivity(newActivityIntent);
                         }
@@ -136,6 +122,109 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, username + " does not exist", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private String createRoomAsHost(String opponentUsername) {
+        // TODO: improve roomID generation
+        String roomId = getCurrentUsername() + opponentUsername + String.valueOf(new Date().getTime());
+
+        Log.d(TAG, "createRoomAsHost: " + roomId);
+
+        Map<String, Object> roomDetails = new HashMap<>();
+        roomDetails.put("host", getCurrentUsername());
+        roomDetails.put("opponent", opponentUsername);
+        roomDetails.put("hostMove", "");
+        roomDetails.put("opponentMove", "");
+        roomDetails.put("startTime", String.valueOf(new Date().getTime()));
+        roomDetails.put("isFull", false);
+        roomDetails.put("isGameOver", false);
+        roomDetails.put("hostScore", 0);
+        roomDetails.put("opponentScore", 0);
+        roomDetails.put("roomId", roomId);
+
+        Log.d(TAG, "createRoomAsHost: " + roomDetails.values() + " " + roomDetails.keySet());
+
+        db.collection("rooms")
+                .document(roomId)
+                .set(roomDetails);
+
+        db.collection("users")
+                .document(getCurrentUsername())
+                .update("roomId", roomId);
+        db.collection("users")
+                .document(opponentUsername)
+                .update("roomId", roomId);
+
+        // TODO: go to new intent
+        return roomId;
+    }
+
+    private void joinRoomAsOpponent(String hostUsername) {
+        db.collection("users")
+                .document(hostUsername)
+                .get()
+
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(final DocumentSnapshot documentSnapshot) {
+                        Log.d(TAG, "onSuccess: " + documentSnapshot.get("name") + " " + documentSnapshot.get("roomId"));
+
+                        db.collection("rooms")
+                                .document((String) documentSnapshot.get("roomId"))
+                                .update("isFull", true)
+
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Intent startNewActivityIntent =
+                                                new Intent(MainActivity.this, GameActivity.class);
+                                        startNewActivityIntent.putExtra("roomId", (String) documentSnapshot.get("roomId"));
+
+                                        startActivity(startNewActivityIntent);
+                                        finish();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG, "onFailure: ROOM UPDATION JOINROOMASOPPONENT FAILED" + e.toString());
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: JOINROOMASOPPONENT GET FAILIRE" + e.toString());
+                    }
+                });
+    }
+
+    private void getPendingInvites() {
+        db.collection("users")
+                .document(getCurrentUsername())
+                .collection("invites")
+
+                .addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        usernames.clear();
+                        listViewDetails.clear();
+                        listView.setAdapter(null);
+
+                        if (queryDocumentSnapshots != null) {
+                            for (QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots) {
+                                usernames.add((String) queryDocumentSnapshot.get("by"));
+                                listViewDetails.add(queryDocumentSnapshot.get("by") + " at " + queryDocumentSnapshot.get("at"));
+                            }
+
+                            ArrayAdapter arrayAdapter =
+                                    new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, listViewDetails);
+
+                            listView.setAdapter(arrayAdapter);
+                        }
+                    }
+                });
     }
 
 //    private boolean checkIfUserExists(String username) {
@@ -162,5 +251,12 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         return currentUser.getEmail().split("@")[0];
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        listView = findViewById(R.id.listView);
+        getPendingInvites();
     }
 }
