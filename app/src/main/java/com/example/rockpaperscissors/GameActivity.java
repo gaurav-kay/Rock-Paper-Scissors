f8a1fd92f77ac2cc00b40aab3450e8f3125e31ee
Gaurav Kashyap <gauravandkashyap@gmail.com>
2019-09-20 01:23:47 +0530

    added code to handle game flow

--- app/src/main/java/com/example/rockpaperscissors/GameActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/GameActivity.java
@@ -7,10 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
 
 import javax.annotation.Nullable;
 
@@ -20,6 +24,7 @@ public class GameActivity extends AppCompatActivity {
     private Bundle bundle = getIntent().getExtras();
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     private Button rockButton, paperButton, scissorButton;
     private TextView scoreTextView, opponentMoveTextView, yourMoveTextView;
@@ -27,6 +32,9 @@ public class GameActivity extends AppCompatActivity {
     private String roomId = null;
     private Integer gameState = null;
     private boolean userPlayed = false;
+    private String opponentMove = null, userMove = null;
+    private String opponentUsername = null, currentUsername = null;
+    private Integer prevCurrentUserScore = 0, prevOtherUserScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,8 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         roomId = bundle.getString("roomId");
+        opponentUsername = bundle.getString("opponentUsername");
+        currentUsername = bundle.getString("hostUsername");
 
         addGameStateListener();
 
@@ -52,12 +62,22 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (view == rockButton) {
-                    // turn played: rock
+                    userMove = "rock";
                 } else if (view == paperButton) {
-
+                    userMove = "paper";
                 } else if (view == scissorButton) {
-
+                    userMove = "scissor";
                 }
+
+                userPlayed = true;
+
+                rockButton.setClickable(false);
+                paperButton.setClickable(false);
+                scissorButton.setClickable(false);
+
+                yourMoveTextView.setText(userMove);
+
+                updateWithUserTurn(userMove);
             }
         };
     }
@@ -77,18 +97,24 @@ public class GameActivity extends AppCompatActivity {
                     .addSnapshotListener(new EventListener<DocumentSnapshot>() {
                         @Override
                         public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                            prevCurrentUserScore = (Integer) documentSnapshot.get(currentUsername + "Score");
+                            prevOtherUserScore = (Integer) documentSnapshot.get(opponentUsername + "Score");
+
                             if (documentSnapshot.get("gameState").equals(0)) {
                                 gameState = 0;
 
                                 askUserToPlayTurn();
-                                disableButtons();
                             } else if (documentSnapshot.get("gameState").equals(1)) {
                                 gameState = 1;
 
                                 if (userPlayed == false) {
                                     updateForOtherUserPlayed();
+
+                                    opponentMove = (String) documentSnapshot.get(opponentUsername + "Turn");  // saving to use for score calculation
                                 } else {
-                                    waitForOtherUserTurn();
+                                    waitForOtherUserTurn(documentSnapshot.get(getCurrentUsername() + "Turn"));
+
+                                    // presently, no update turn feature
                                 }
                             } else if (documentSnapshot.get("gameState").equals(2)) {
                                 gameState = 2;
@@ -101,8 +127,81 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
+    private void updateForRoundFinish() {
+        // pass 
+    }
+
+    private void calculateScores() {
+        boolean currentUserWins = false;
+        if (userMove.equals(opponentMove)) {
+            currentUserWins = false;
+        } else if (userMove.equals("rock") && opponentMove.equals("scissor")) {
+            currentUserWins = true;
+        } else if (userMove.equals("paper") && opponentMove.equals("rock")) {
+            currentUserWins = true;
+        } else if (userMove.equals("scissor") && opponentMove.equals("paper")) {
+            currentUserWins = true;
+        }
+
+        if (currentUserWins) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("gameState", 0);
+            map.put(currentUsername + "Score", prevCurrentUserScore + 1);
+            prevCurrentUserScore += 1;
+
+            db.collection("rooms")
+                    .document(roomId)
+
+                    .update(map);
+        } else {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("gameState", 0);
+            map.put(opponentUsername + "Score", prevOtherUserScore + 1);
+            prevOtherUserScore += 1;
+
+            db.collection("rooms")
+                    .document(roomId)
+
+                    .update(map);
+        }
+
+        scoreTextView.setText(currentUsername + ": " + prevCurrentUserScore + ", " + opponentUsername + ": " + prevOtherUserScore);
+    }
+
+    private void waitForOtherUserTurn(Object move) {
+        yourMoveTextView.setText("Your Turn: " + move);
+
+        // set loading screen maybe
+    }
+
+    private void updateForOtherUserPlayed() {
+        opponentMoveTextView.setText("Opponent has played");
+
+        // can only think of this happening till now
+    }
+
     private void askUserToPlayTurn() {
         yourMoveTextView.setText("Play your Move");
 
+        rockButton.setClickable(true);
+        paperButton.setClickable(true);
+        scissorButton.setClickable(true);
+    }
+
+    private void updateWithUserTurn(String move) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("gameState", 1);
+        map.put(getCurrentUsername() + "Turn", move);
+
+        db.collection("rooms")
+                .document(roomId)
+
+                .update(map);
+    }
+
+    private String getCurrentUsername() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        return currentUser.getEmail().split("@")[0];
     }
 }
